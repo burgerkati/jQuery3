b184666f703abe410dcd8ba99c9e1a2b7cdaf93b
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-04-29 12:04:52 +0200

    adjust l.5-8

--- exercice_2/main.js
+++ exercice_2/main.js
@@ -2,10 +2,10 @@
 Create two buttons: one to increment, the other to decrement
 An input retrieves the values
 */
-var input="<input type='text' id='input'>";
-var addButton="<button id='clickToAddOne'>Add One</button>";
-var subtractButton="<button id='clickToSubtractOne'>Subtract One</button>";
-var count= 0;
+var input = "<input type='text' id='input'>";
+var addButton = "<button id='clickToAddOne'>Add One</button>";
+var subtractButton = "<button id='clickToSubtractOne'>Subtract One</button>";
+var count = 0;
 
 $(document).ready(function(){
   $("body").prepend(input,addButton,subtractButton);
